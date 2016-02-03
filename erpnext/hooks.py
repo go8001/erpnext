dad3db244cc5e8606e898b0d5fba7de04b3b0d13
david <david.li@infocito.com>
2016-02-03 09:31:14 +0800

    更改logo，邮件的底部标示

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -132,8 +132,8 @@ scheduler_events = {
 }
 
 default_mail_footer = """<div style="text-align: center;">
-	<a href="https://erpnext.com?source=via_email_footer" target="_blank" style="color: #8d99a6;">
-		Sent via ERPNext
+	<a href="" target="_blank" style="color: #8d99a6;">
+		Sent via Infocito
 	</a>
 </div>"""
 
